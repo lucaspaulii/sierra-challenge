a0b04cb21277eb66867e250678d71d22cbe48533
lucaspaulii <lucaspauli22@gmail.com>
2023-08-30 18:32:08 -0300

    feat: createTask and updateTaskAndLocalStorage functions added to useTasks hook

--- src/utils/hooks/useTasks.ts
+++ src/utils/hooks/useTasks.ts
@@ -2,6 +2,23 @@ import { useState } from "react";
 
 export const useTasks = () => {
     const [tasks, setTasks] = useState([] as Task[]);
+
+    const createTask = (taskName: string) => {
+        const newTask: Task = {
+            id: 1,
+            name: taskName,
+            status: "todo"
+        }
+        const newTasks = [newTask, ...tasks];
+        updateTasksAndLocalStorage(newTasks)
+    }
+
+    function updateTasksAndLocalStorage(updatedTasks: Task[]) {
+        setTasks([...updatedTasks]);
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      }
+
+    return { tasks, createTask }
 }
 
 export type Task = {
