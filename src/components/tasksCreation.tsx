7de8f94dbb6866d735cd00fe34836de5f6300be3
lucaspaulii <lucaspauli22@gmail.com>
2023-08-30 18:27:56 -0300

    feat: tasks creation component structured

--- src/components/tasksCreation.tsx
+++ src/components/tasksCreation.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 export default function TasksCreation() {
-    return (
-        <div></div>
-    )
-};
+  const [taskName, setTaskName] = useState("");
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (taskName.length < 2) {
+      return;
+    }
+    if (event.key == "Enter") {
+      // Handle Task creation via useTasks
+    }
+  };
+
+  return (
+    <input
+      id="createTask"
+      type="text"
+      placeholder="Add todo item"
+      autoComplete="off"
+      className="w-full border-b border-l-0 border-r-0 border-t-0 border-charcoal-600 bg-transparent text-xl placeholder:text-white placeholder:opacity-60 focus:rounded focus:border-none"
+      value={taskName}
+      onChange={(e) => {
+        setTaskName(e.target.value);
+      }}
+      onKeyDown={handleKeyDown}
+    />
+  );
+}
